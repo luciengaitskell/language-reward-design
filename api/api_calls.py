8f4ba1a7a0074ebb360a14b606fdf029d1a92c40
Kai McClennen <kmcclenn@mit.edu>
2024-04-25 13:01:14 -0400

    added env notebook

--- api/api_calls.py
+++ api/api_calls.py
@@ -1,23 +1,11 @@
- messages = [
+def openai_repr(prompt):
+    '''
+    Query the OpenAI API to get the response to the prompt. Prompt something like
+    
+     messages = [
         {"role": "system", "content": "You are a helpful, analytic assistant who can summarize text well."},
         {"role": "user", "content": prompt}
     ]
-
-    # Return the messages list
-    #return messages
-
-def openai_repr(model, docs, topic, doc_to_topic):
-    '''
-    Gets the ChatGPT representation of a topic.
-
-    Args:
-        model (object): The bertopic model.
-        docs (object): The training corpus.
-        topic (int): The topic number.
-        doc_to_topic (object): The DataFrame relating a document to a topic.
-
-    Returns:
-        object: The openai response.
     '''
     import openai
 
@@ -26,22 +14,6 @@ def openai_repr(model, docs, topic, doc_to_topic):
 
     # Define the GPT model
     gpt_model = 'gpt-3.5-turbo'
-
-    # Get document information
-    doc_info = model.get_document_info(docs)
-
-    # Filter documents by topic
-    topic_df = doc_info.loc[doc_info["Topic"] == topic]
-
-    # Get topic representation and number
-    topic_words = topic_df["Representation"].tolist()[0]
-    topic_num = topic_df["Topic"].tolist()[0]
-
-    # Get documents related to the topic
-    topic_docs = doc_to_topic.loc[doc_to_topic["Topic"] == topic_num]["Document"].tolist()
-
-    # Create prompt for GPT model
-    prompt = gpt_prompt(topic_docs, topic_words)
 
     # Generate response using Chat Completion API
     response = openai.ChatCompletion.create(
