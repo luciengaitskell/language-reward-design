212fda36b56ddaaa5de6fec11ca6479f7bfe3e97
Luc Gaitskell <lucg@mit.edu>
2024-05-02 23:48:02 -0400

    Add configurable parking env

--- wiley/custom_parking.py
+++ wiley/custom_parking.py
@@ -1,19 +1,17 @@
 # custom_parking_env.py
-from highway_env.envs.parking_env import ParkingEnv
+from typing import Callable
+
+from highway_env.envs import ParkingEnv
+
 import numpy as np
-from gym.envs.registration import register
+
 
 class CustomParkingEnv(ParkingEnv):
-    def __init__(self, *args, **kwargs):
+    def __init__(
+        self,
+        *args,
+        compute_reward: Callable[[np.ndarray, np.ndarray, dict, float], float],
+        **kwargs
+    ):
         super().__init__(*args, **kwargs)
-
-    def _reward(self, action):
-        cur_pos = self.vehicle.position
-        desired_pos = self.goal_position
-        reward = -np.linalg.norm(cur_pos - desired_pos)
-        return reward
-    
-register(
-    id='CustomParking-v0',
-    entry_point='custom_parking_env:CustomParkingEnv',
-)
+        self.compute_reward = compute_reward
