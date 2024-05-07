3b6273a6c100d0a8fe7f8f73d2a494041230d8e0
Kai McClennen <kmcclenn@mit.edu>
2024-05-07 01:47:13 -0400

    got llm reward trained on minigrid

--- kai/custom_minigrid.py
+++ kai/custom_minigrid.py
@@ -13,21 +13,19 @@ class CustomMinigridEnv(LockedRoomEnv):
     def __init__(
         self,
         *args,
-        compute_reward: Callable[["CustomMinigridEnv", spaces.Dict, dict], float],
+        compute_reward: Callable[["CustomMinigridEnv", spaces.Dict], float],
         **kwargs
     ):
         self.compute_reward_func = compute_reward
         super().__init__(*args, **kwargs)
         
-    def compute_reward(self, current_state: spaces.Dict, info: dict) -> float:
-        return self.compute_reward_func(self, current_state, info)
+    def compute_reward(self, current_state: spaces.Dict) -> float:
+        return self.compute_reward_func(self, current_state)
 
 
-register(
-    id="CustomLockedRoom-v0",
-    entry_point="custom_lockedroom:CustomMinigridEnv",
-    # entry_point='highway_env.envs:ParkingEnv',
-)
+
+register( id="CustomLockedRoom-v0", entry_point=CustomMinigridEnv)
+    # entry_point='highway_env.envs:ParkingEnv')
 
 ### to make work with VLM, we will have to customize our reward as something based on colors, etc.
 ### eg VLM1 -> color purple is best, higher reward in that direction, VLM2 -> translate that into code; distance from purple coordinate in observation, distance from key, distance from door
